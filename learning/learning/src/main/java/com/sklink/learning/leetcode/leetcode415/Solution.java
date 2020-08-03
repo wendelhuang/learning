20496548552eca6c97863f7b8720b2b7bf4e1dd3
huangww <huangww@hsfund.com>
2020-08-03 08:51:29 +0800

    ADD: leetcode 415

---
+++ learning/learning/src/main/java/com/sklink/learning/leetcode/leetcode415/Solution.java
@@ -0,0 +1,11 @@
+package com.sklink.learning.leetcode.leetcode43;
+
+class Solution {
+	public String multiply(String num1, String num2) {
+
+    }
+	
+	public static void main(String[] args) {
+		System.out.println(new Solution().multiply("123", "456"));
+	}
+}
